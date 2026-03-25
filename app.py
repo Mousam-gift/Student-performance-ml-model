9a4525b618189cc6d55219959e0c9ddc2c23ef00
Mousam Das <209365734+Mousam-gift@users.noreply.github.com>
2026-03-26 00:45:37 +0530

    Refactor model loading and performance prediction logic

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ import pickle
 
 #load trained model
 with open ("student_performance_model.pkl","rb")as file:
-    model = pickle.load(file)
+    data = pickle.load(file)
+    model = data["model"]
+    coding_encoder = data["coding_encoder"]
+    performance_encoder = data["performance_encoder"]
 
 #career recomendation logic
 def recommend_career(math_score, science_score, coding_interest,performance):
-    if performance==1 and coding_interest==1:
+    if performance=="Good" and coding_interest==1:
         return "Software Developer / Data Science"
-    elif performance==1 and science_score>=75:
+    elif performance=="Good" and science_score>=75:
         return "Engineering / Research"
-    elif performance==0:
+    elif performance=="Average":
         return "Skill-based IT / Support roles"
     else:
         return "Foundation Courses / Diploma"
@@ -26,23 +29,15 @@ math_score = st.slider("Math Score",35,100,70)
 science_score = st.slider("Science Score",35,100,70)
 coding_interest = st.selectbox("Coding Interest",["Yes","No"])
 
-if coding_interest == "Yes":
-    coding_interest_val = 1
-else:
-    coding_interest_val = 0
 
 #predict button
 if st.button("predict"):
+    coding_interest_val = coding_encoder.transform([coding_interest])[0]
     input_data = pd.DataFrame([[attendance, math_score, science_score, coding_interest_val]], columns=["attendance", "math_score", "science_score", "coding_interest"])
     prediction = model.predict(input_data)[0]
 
-    if prediction == 1:
-        performance = "Good"
-    elif prediction == 0:
-        performance = "Average"
-    else:
-        performance = "Poor"
+    performance = performance_encoder.inverse_transform([prediction])[0]
 
-    career = recommend_career(math_score, science_score, coding_interest_val, prediction)
+    career = recommend_career(math_score, science_score, coding_interest_val, performance)
     st.success(f"Predicted Performance:{performance}")
     st.info(f"Recommended Career Path: {career}")    
